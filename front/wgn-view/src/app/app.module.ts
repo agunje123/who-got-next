939079351c0d1f2f78a11746116d9792cc242ddb
agunje123 <andrija.gunjevic1@gmail.com>
2023-10-10 14:43:38 +0200

    Add material module to codebase and app module

--- front/wgn-view/src/app/app.module.ts
+++ front/wgn-view/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './modules/material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { NavbarComponent } from './components/navigation/navbar/navbar.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
