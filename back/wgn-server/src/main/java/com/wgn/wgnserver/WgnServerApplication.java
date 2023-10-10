b964a9b63edce88aa0eb35dd9b273958291e93d5
agunje123 <andrija.gunjevic1@gmail.com>
2023-10-10 14:16:22 +0200

    Generate angular and spring boot applications

---
+++ back/wgn-server/src/main/java/com/wgn/wgnserver/WgnServerApplication.java
@@ -0,0 +1,13 @@
+package com.wgn.wgnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgnServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgnServerApplication.class, args);
+	}
+
+}
