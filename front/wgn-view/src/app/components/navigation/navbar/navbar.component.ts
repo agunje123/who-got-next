f794442507fae1860e717764f532290e80300729
agunje123 <andrija.gunjevic1@gmail.com>
2023-10-10 17:50:54 +0200

    Create dark and light theme, toggle in navbar

--- front/wgn-view/src/app/components/navigation/navbar/navbar.component.ts
+++ front/wgn-view/src/app/components/navigation/navbar/navbar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  darkModeOn: boolean = true;
 
+  toggleDarkMode() {
+    if (this.darkModeOn === true) {
+      this.darkModeOn = false;
+    } else {
+      this.darkModeOn = true;
+    }
+  }
 }
