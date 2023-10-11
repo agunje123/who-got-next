f80d90a1a24a0a230b6dbaa9971e329c6bfc3e20
agunje123 <andrija.gunjevic1@gmail.com>
2023-10-11 12:43:05 +0200

    Create map service

--- front/wgn-view/src/app/app.module.ts
+++ front/wgn-view/src/app/app.module.ts
@@ -9,19 +9,19 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapComponent } from './components/map/map/map.component';
 
+//SERVICES
+import { SettingsService } from './services/settings.service';
+import { MapService } from './services/map.service';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    MapComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, MapComponent],
   imports: [
     BrowserModule,
     MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [SettingsService, MapService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
