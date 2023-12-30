0e93d98c257d88c4e83d63ab722b1ae72db6c0a5
agunje123 <andrija.gunjevic1@gmail.com>
2023-12-30 18:42:31 +0100

    Add supabase

---
+++ front/wgn-view/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://mzrdwwcnexusyokhurdx.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16cmR3d2NuZXh1c3lva2h1cmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM5NTU1MzAsImV4cCI6MjAxOTUzMTUzMH0.wZ68ktzzjn4lnOveqNtqqtrWCZ9yinc3OaXXND1uBSA',
+};
